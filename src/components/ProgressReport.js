828148d4cdc9b0b1505d3fe57fa0eb21bad22bf6
yousscharif <youssecharif15@gmail.com>
2024-04-03 13:05:25 +0300

    Table

--- src/components/ProgressReport.js
+++ src/components/ProgressReport.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { Typography, Paper, List, ListItem, ListItemText, Box, MenuItem, InputLabel, Select } from '@mui/material';
+import { Typography, Paper, List, ListItem, ListItemText, Box, MenuItem, InputLabel, Select, Table, TableCell, TableHead, TableRow, TableBody } from '@mui/material';
 
 import { getUserToken } from '../localStorage';
 import Nav from './Nav';
@@ -21,6 +21,7 @@ function ProgressReport() {
         employees.forEach( (e) => { if (e["employee_name"] == name) {setSelectedEmployee(e);} } );
         fetchEmployeeTasks();
     };
+
 
     const fetchEmployees = useCallback(() => {
         fetch(`${SERVER_URL}/employee`, {
@@ -72,10 +73,11 @@ function ProgressReport() {
   return (
     <div>
         <Nav />
-        <div>
+       
+        <Box sx={{height:'200px',marginLeft:'2%'}}> 
             <InputLabel 
                 id="select-employee-label"
-                sx={{ fontSize: 24 }}
+                sx={{ fontSize: 24, height:'100px', display:'flex',flexDirection:'column',justifyContent:'center' }}
                 >Select an employee</InputLabel>
             <Select
                 labelId="select-employee-label"
@@ -90,28 +92,36 @@ function ProgressReport() {
                     <MenuItem key={index} value={e}>{e}</MenuItem>
                 ))}
             </Select>
-        </div>
-        <Typography variant="h4" gutterBottom>
+           
+   
+        <Typography variant="h4" gutterBottom sx={{paddingTop:'2%'}}>
             Employee Progress Report
         </Typography>
-        <Typography variant="body1"><strong>Phone Number:</strong> { selectedEmployee["employee_phone"] }</Typography>
-        <Box>
-            <Typography variant="h5">Tasks:</Typography>
-            <List>
-            {tasks.map((task, index) => (
-                <Box sx={{display:'flex',flexDirection:'column'}}>
-                    {/* <ListItemText primary={task["task_name"]} secondary={`Progress: ${task["task_percentage"]}`}     
-                     /> */}
-                    <Typography variant="h6">Task: {task["task_name"]}</Typography>
-                    <Typography variant="h7">Last commit: {task["task_commit"]}</Typography>
-                    <Typography variant="h7">Progress: {task["task_percentage"]}</Typography>
-                    <Typography variant="h7">Deadline: {task["task_deadline"]}</Typography>
-                </Box>
-                
-            ))}
-            </List>
-        </Box>
+
+        <Table  aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Task</TableCell>
+            <TableCell align="right">Last Commit</TableCell>
+            <TableCell align="right">Progress</TableCell>
+            <TableCell align="right">Deadline</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+         
+            <TableRow key={tasks["task_name"]}>
+
+              <TableCell align="right">{tasks["task_name"]}</TableCell>
+              <TableCell align="right">{tasks["task_commit"]}</TableCell>
+              <TableCell align="right">{tasks["task_percentage"]}</TableCell>
+              <TableCell align="right">{tasks["task_deadline"]}</TableCell>
+            </TableRow>
+      
+        </TableBody>
+      </Table>
+      </Box>
     </div>
+  
   )
 }
 
